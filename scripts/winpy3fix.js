502aa01ab9082a4ccb5ef747c561ddb77a2cd6e4
przemyslawpluta <przemekpluta@hotmail.com>
2014-04-08 22:05:07 +0100

    typo

--- scripts/winpy3fix.js
+++ scripts/winpy3fix.js
@@ -1,7 +1,15 @@
 var mv = require('mv'),
+	fs = require('fs'),
     path = require('path');
 
-mv(path.resolve(__dirname + '../' + '../.temp/youtube-dl.js'), path.resolve(__dirname + '../' + '../node_modules/youtube-dl/lib/youtube-dl.js'), function error(err) {
-    'use strict';
-    if (err !== null) { console.log(err); }
+var target = path.resolve(__dirname + '../' + '../node_modules/youtube-dl/lib/youtube-dl.js');
+
+fs.unlink(target, function (err) {
+	'use strict';
+  	if (err !== null) { return console.log(err.stack); }
+	mv(path.resolve(__dirname + '../' + '../.temp/youtube-dl.js'), target, function error(err) {
+	    if (err !== null) { console.log(err); }
+	});
 });
+
+

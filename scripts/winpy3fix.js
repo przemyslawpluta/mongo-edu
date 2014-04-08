8877218526142e068ee98c866b74aea528d66dc0
przemyslawpluta <przemekpluta@hotmail.com>
2014-04-08 22:18:53 +0100

    win specific

--- scripts/winpy3fix.js
+++ scripts/winpy3fix.js
@@ -1,15 +1,18 @@
 var mv = require('mv'),
-	fs = require('fs'),
+    fs = require('fs'),
     path = require('path');
 
-var target = path.resolve(__dirname + '../' + '../node_modules/youtube-dl/lib/youtube-dl.js');
+var isWin = /^win/.test(process.platform);
 
-fs.unlink(target, function (err) {
-	'use strict';
-  	if (err !== null) { return console.log(err.stack); }
-	mv(path.resolve(__dirname + '../' + '../.temp/youtube-dl.js'), target, function error(err) {
-	    if (err) { console.log(err); }
-	});
-});
+if (isWin) {
+    var target = path.resolve(__dirname + '../' + '../node_modules/youtube-dl/lib/youtube-dl.js');
 
-
+    fs.unlink(target, function (err) {
+        'use strict';
+        if (err !== null) { return console.log(err.stack); }
+        mv(path.resolve(__dirname + '../' + '../.temp/youtube-dl.js'), target, function error(err) {
+            if (err) { console.log(err); }
+            console.log('win py3.x fix');
+        });
+    });
+}

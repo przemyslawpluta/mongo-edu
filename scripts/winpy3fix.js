1ddd2de23d9ae93511da942c2dc568ffba7fa8a1
przemyslawpluta <przemekpluta@hotmail.com>
2014-04-08 22:02:02 +0100

    typo

--- scripts/winpy3fix.js
+++ scripts/winpy3fix.js
@@ -3,5 +3,5 @@ var mv = require('mv'),
 
 mv(path.resolve(__dirname + '../' + '../.temp/youtube-dl.js'), path.resolve(__dirname + '../' + '../node_modules/youtube-dl/lib/youtube-dl.js'), function error(err) {
     'use strict';
-    if (err !== null) { console.log(err.stack); }
+    if (err !== null) { console.log(err); }
 });
